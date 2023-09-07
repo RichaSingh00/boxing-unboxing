15305b9f5ba82b21eb22d5f32832e54a54f6d43b
RichaSingh00 <ririco613@gmail.com>
2023-09-07 17:20:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10; //value type
+            object boxedNum = num; //implicit conversion from value to ref
+            int unboxedNum = (int)boxedNum; //explicit conversion from ref to value
+          
+            Console.WriteLine("Original number (integer) is:" +num);
+            Console.WriteLine("Boxed number (object) is: "+boxedNum); 
+            Console.WriteLine("Unboxed number (integer) is: "+unboxedNum);
+           
+            //Used for methods where a specific type is only accepted hence the need for conversion
+        }
+    }
+}
